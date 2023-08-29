526b7582fe2ea4eeeee2e3e86a6f91b62ff02402
Prajwal Kene <prajwalnk123@gmail.com>
2023-08-29 17:20:35 +0530

    updating

--- index.js
+++ index.js
@@ -3,63 +3,43 @@ const http = require('http');
 const socketIo = require('socket.io');
 const crypto = require('crypto');
 const Influx = require('influx');
+const path = require('path');
+const { DataController } = require('./controllers/dataController');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Replace these values with your own secret key and IV
-const secretKey = 'your-secret-key';
-const iv = 'your-iv';
-
-// In a real scenario, store these sensitive information securely
-// Initialize the InfluxDB instance
+// InfluxDB setup
 const influx = new Influx.InfluxDB({
   host: 'localhost',
-  database: 'your-database-name',
+  database: 'your_database_name', // Change this to your InfluxDB database name
   schema: [
     {
       measurement: 'data',
       fields: {
-        value: Influx.FieldType.FLOAT,
+        // Define your fields here
+        name: Influx.FieldType.STRING,
+        city: Influx.FieldType.STRING,
       },
-      tags: ['source'],
+      tags: [],
     },
   ],
 });
 
-io.on('connection', (socket) => {
-  console.log('A user connected');
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
 
-  // Simulate generating encrypted data
-  const originalData = 42;
-  const cipher = crypto.createCipheriv('aes-256-cbc', secretKey, iv);
-  let encryptedData = cipher.update(String(originalData), 'utf8', 'hex');
-  encryptedData += cipher.final('hex');
-
-  // Emit encrypted data to frontend
-  socket.emit('encryptedData', encryptedData);
-
-  // Listen for decrypted data from frontend
-  socket.on('decryptedData', async (data) => {
-    console.log('Received decrypted data:', data);
-
-    // Decode and save to InfluxDB
-    const decodedData = parseFloat(data);
-    await influx.writePoints([
-      {
-        measurement: 'data',
-        fields: { value: decodedData },
-        tags: { source: 'frontend' },
-      },
-    ]);
-
-    // Emit saved data to frontend
-    io.emit('savedData', decodedData);
-  });
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  DataController.emitData(socket, influx);
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
