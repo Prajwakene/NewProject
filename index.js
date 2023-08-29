02dd178fb6f04115203a8dc84f3efa09b002458a
Prajwal Kene <prajwalnk123@gmail.com>
2023-08-29 16:40:35 +0530

    ran the server

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-const app =require("express");
+// import all the requires packages whatever is necessary 
+//1
+const express = require('express');
+//2
+const app = express();
+//3
+const port = 8000;
 
+
+//4 start the server so that we can listen req and response 
+app.listen(process.env.PORT, function(err){
+    if (err){
+        console.log(`Error in running the server: ${err}`);
+    }
+
+    console.log(`Server is running on port: ${port}`);
+});
