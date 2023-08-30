32725178166cd436b7ca5a8ac88fd841d08de06d
Prajwal Kene <prajwalnk123@gmail.com>
2023-08-30 14:56:13 +0530

    updating

--- models/Data.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const dataSchema = new mongoose.Schema({
-  name: String,
-  city: String,
-  timestamp: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('Data', dataSchema);
